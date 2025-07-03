66a1b49502c43a669fd6ba7abbed7d57d0590a23
LeandroUTNFRA <leandroes2403@gmail.com>
2025-07-03 13:33:53 -0300

    implementacion EJS

--- index.js
+++ index.js
@@ -2,13 +2,23 @@ import express from "express";
 import enviroments from "./src/api/config/enviroments.js";
 import cors from "cors";
 import { loggerUrl } from "./src/api/middlewares/middlewares.js";
-import { productRoutes } from "./src/api/routes/index.js";
+import { productRoutes, viewRoutes } from "./src/api/routes/index.js";
+import { __dirname, join } from "./src/api/utils/index.js";
 
 
 const PORT = enviroments.port;
 const app = express();
 
 
+
+// Configuramos EJS como motor de plantillas
+app.set("view engine", "ejs");
+
+// Le indicamos a la aplicacion desde que ruta va a servir vistas desde raizProyecto/src/views
+app.set("views", join(__dirname, "src/views"));
+
+// Middleware para servir archivos estaticos
+app.use(express.static(join(__dirname, "src/public")));
 
 
 // Middlewares //
@@ -28,7 +38,8 @@ app.get("/", (req, res) => {
 // Productos
 app.use("/api/productos", productRoutes);
 
-
+// Rutas de las vistas EJS
+app.use("/dashboard", viewRoutes);
 
 app.listen(PORT, () => {
     console.log(`servidor activo en el puerto ${PORT}`);
